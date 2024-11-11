7490fc2e8162a903a0335489d7e8c815bc4ad3a4
Danish Siddiqui <danbot555@gmail.com>
2024-11-10 21:05:45 -0500

    Setup of express.js server

---
+++ stock_market_sim/backend/src/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const PORT = process.env.PORT || 3000;
+
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node.js backend!');
+  });
+  
+
+
+
+// API Routes
+
+app.get('/api/test', (req, res) => res.send( {
+    message: 'testing!'
+}))
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  

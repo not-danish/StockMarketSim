62fc2fdd06804354aaf9916b12ec8be975993e22
Danish Siddiqui <danbot555@gmail.com>
2024-11-14 13:02:53 -0500

    fixed tailwind issue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Make sure to import your router instance
+import './assets/main.css'; // Make sure this points to the correct file with Tailwind imports
+
 
 createApp(App).use(router).mount('#app');
